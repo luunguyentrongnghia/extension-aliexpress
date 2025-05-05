96d6edb8b6082c0ebd20878669ee0387b9d84f4c
luunguyentrongnghia <luutrongnghia281102@gmail.com>
2025-05-05 18:08:28 +0700

    upcode 5/5

--- content/btnallScript.js
+++ content/btnallScript.js
@@ -1,11 +1,13 @@
 let htmlTemplate = `<div class="import-button-container">
-    <button class="import-button">Import all products</button>
+    <button class="import-button" style="display: flex; gap: 5px;">
+     <div class="spinner-border spinner-border-sm" role="status" style="display: none;"></div> <div> Import all products</div>
+    </button>
 </div>`;
 let link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
 link.href = chrome.runtime.getURL('css/btnallScript.css');
-window.onload = () => {
+window.onload = async() => {
     const wrapperObject = document.querySelector("#root");
     console.log(wrapperObject);
     
@@ -15,21 +17,54 @@ window.onload = () => {
         
         // Lắng nghe sự kiện click để thực hiện logic lấy dữ liệu sản phẩm
         const importButton = wrapperObject.querySelector(".import-button");
+        const { accessToken } = await chrome.storage.local.get('accessToken');
+        const { refreshToken } = await chrome.storage.local.get('refreshToken');
         importButton.addEventListener("click",async () => {
-            console.log("Fetching product data...");
-            await scrapeProductsData();
+            if (!refreshToken) {
+                alert(' Vui lòng đăng nhập.');
+                return; 
+            }
+            if(isAccessTokenExpired(accessToken)){
+                await refreshAccessToken();
+            }
+            importButton.classList.add('loading')
+            loadingContainer.style.display = "block";
         });
     }
 };
+async function refreshAccessToken() {
+    const { refreshToken } = await chrome.storage.local.get('refreshToken');  // Lấy refresh token từ storage
 
-async function scrapeProductsData() {
-    const productLinks = document.querySelectorAll('.hm_bl a');  // Các link sản phẩm
-
-    // Gửi yêu cầu mở các tab mới cho từng sản phẩm
-    productLinks.forEach((link) => {
-        chrome.runtime.sendMessage({
-            action: "openTab",  // Gửi yêu cầu mở tab mới
-            url: link.href
-        });
+    const response = await fetch('http://localhost:8000/api/token/refresh/', {  // Gọi API refresh token
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            refresh: refreshToken
+        })
     });
+
+    if (!response.ok) {
+        throw new Error('Failed to refresh token');
+    }
+
+    const data = await response.json();
+    await chrome.storage.local.set({
+        accessToken: data.access,
+        refreshToken: data.refresh
+    });
+
+    return data.access;  // Trả về access token mới
 }
+// async function scrapeProductsData() {
+//     const productLinks = document.querySelectorAll('.hm_bl a');  // Các link sản phẩm
+
+//     // Gửi yêu cầu mở các tab mới cho từng sản phẩm
+//     productLinks.forEach((link) => {
+//         chrome.runtime.sendMessage({
+//             action: "openTab",  // Gửi yêu cầu mở tab mới
+//             url: link.href
+//         });
+//     });
+// }
